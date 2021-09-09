08304f48d436026bb8a7e5269de99f573fb6b491
Neeladri Sarkar <neeladrirahul@gmail.com>
2021-09-09 16:14:43 +0530

    Adding updated main file

--- src/main/java/com/Neeladri/JenkinsIntegration/App.java
+++ src/main/java/com/Neeladri/JenkinsIntegration/App.java
@@ -1,13 +1,11 @@
 package com.Neeladri.JenkinsIntegration;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println(6+5);
+        System.out.println(77*99);
     }
 }
